9624872794dfff060a3bb07016742ac1db898637
cipherbeta <ikedoud45@gmail.com>
2018-12-24 04:07:14 -0600

    Updates

---
+++ precache-manifest.1c29086b7b2dd26059e4afbc7012c919.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "51bd2a3e1bb4630126034352570159e4",
+    "url": "/Phonetic-Alphabet-Trainer/static/media/carbon.51bd2a3e.png"
+  },
+  {
+    "revision": "f7b822c6adb36766c303",
+    "url": "/Phonetic-Alphabet-Trainer/static/js/runtime~main.f7b822c6.js"
+  },
+  {
+    "revision": "e102b219f009e2816978",
+    "url": "/Phonetic-Alphabet-Trainer/static/js/main.e102b219.chunk.js"
+  },
+  {
+    "revision": "3fa6248a5af13ad4d664",
+    "url": "/Phonetic-Alphabet-Trainer/static/js/1.3fa6248a.chunk.js"
+  },
+  {
+    "revision": "e102b219f009e2816978",
+    "url": "/Phonetic-Alphabet-Trainer/static/css/main.90360da3.chunk.css"
+  },
+  {
+    "revision": "3fa6248a5af13ad4d664",
+    "url": "/Phonetic-Alphabet-Trainer/static/css/1.1bad5a4a.chunk.css"
+  },
+  {
+    "revision": "52b2a7344e3e40b04577a33e3ef80788",
+    "url": "/Phonetic-Alphabet-Trainer/index.html"
+  }
+];
